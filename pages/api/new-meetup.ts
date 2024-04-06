63e010786febcd00b5a4f640ad172596ca665473
rcarlosjorge <carlosepjorge@gmail.com>
2024-04-06 17:42:38 -0400

    new handler

--- pages/api/new-meetup.ts
+++ pages/api/new-meetup.ts
@@ -13,21 +13,18 @@ export default async function handler(
   if (req.method === "POST") {
     const data = req.body;
 
-    const client = await connectToDatabase();
+    try {
+      const client = await connectToDatabase();
+      const db = client.db();
+      const meetupsCollection = db.collection("meetups");
+      const result = await meetupsCollection.insertOne(data);
+      console.log(result);
+      client.close();
 
-    const db = client.db();
-
-    const meetupsCollection = db.collection("meetups");
-
-    const result = await meetupsCollection.insertOne(data);
-
-    console.log(result);
-
-    client.close();
-
-    res
-      .status(201)
-      .json({ message: "Meetup was Added to the DB", meetup: result });
+      res.status(201).json({ message: "Meetup was Added to the DB" });
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
   }
 
   res.status(200).json({ message: "Hello from Next.js!" });
